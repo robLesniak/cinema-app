80e25c8b11a1770c7e9d2fe183908c12e184f074
Michał Nowakowski <michal951@poczta.onet.pl>
2019-03-22 00:27:22 +0100

    add font awesome to names in navbar

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,82 +1,65 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const userIsNotAuthenticatedNav = (
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav nav-pills mr-auto">
+          <li className="nav-item nav-divider">
+            <Link to="" className="nav-link">
+              <i class="fa fa-film" />
+              &nbsp;Repertoire
+            </Link>
+          </li>
+          <li className="nav-item nav-divider">
+            <Link to="" className="nav-link">
+              <i class="fa fa-money" />
+              &nbsp;Price list
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="" className="nav-link">
+              About us&nbsp;
+              <i class="fa fa-question-circle-o" />
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to="/register" className="nav-link ">
+              Sing up
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
     return (
       <nav
-        className="navbar navbar-expand-lg navbar-light "
-        style={{ backgroundColor: "#0267E3" }}
+        className="navbar navbar-expand-sm navbar-light "
+        style={{ backgroundColor: "#7070EF" }}
       >
-        <Link to="" className="navbar-brand">
-          Xyz Cinema
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link to="" className="nav-link">
-                Home <span className="sr-only">(current)</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="" className="nav-link">
-                Link
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link
-                to=""
-                className="nav-link dropdown-toggle"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </Link>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <Link className="dropdown-item" href="#">
-                  Action
-                </Link>
-                <Link className="dropdown-item" href="#">
-                  Another action
-                </Link>
-                <div className="dropdown-divider" />
-                <Link className="dropdown-item" href="#">
-                  Something else here
-                </Link>
-              </div>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
+        <div className="container">
+          <Link to="" className="navbar-brand">
+            <img
+              src={require("../../images/cinema.svg.png")}
+              className="d-inline-block align-top"
+              style={{ height: "32px" }}
+              alt=""
             />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
+          </Link>
+          {userIsNotAuthenticatedNav}
         </div>
       </nav>
     );
   }
 }
+
+export default Header;
