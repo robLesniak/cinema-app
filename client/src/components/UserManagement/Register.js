bfb1ab0facf3c22cfb1e9f0f1574e654028dfb8b
Robert Lesniak <robert.lesniak@thecogworks.com>
2019-03-27 17:37:20 +0100

    refactor register form

--- client/src/components/UserManagement/Register.js
+++ client/src/components/UserManagement/Register.js
@@ -5,7 +5,8 @@ class Register extends Component {
   state = {
     username: '',
     email: '',
-    password: ''
+    password: '',
+    confirmPassword: ''
   }
 
   handleChange = (event) => {
@@ -17,28 +18,39 @@ class Register extends Component {
   render() {
     const { username, email, password } = this.state;
     return (
-      <div>
-        <input
-          name="username"
-          placeholder="Username"
-          onChange={this.handleChange} 
-          value={username}
-        />
-        <input
-          name="email" 
-          placeholder="email"
-          type="email" 
-          onChange={this.handleChange}
-          value={email}
-        />
-        <input 
-          name="password" 
-          type="password" 
-          placeholder="password" 
-          onChange={this.handleChange}
-          value={password}
-        />
+      <div class="container-fluid bg-light py-3">
+        <div class="row">
+          <div class="col-md-6 mx-auto">
+            <div class="card card-body">
+              <h3 class="text-center mb-4">Sign-up</h3>
+                {/* <div class="alert alert-danger">
+                  <a class="close font-weight-light" data-dismiss="alert" href="#">×</a>Password is too short.
+                </div> */}
+                <fieldset>
+                  <div class="form-group has-success">
+                    <input class="form-control input-lg" placeholder="Username" name="username" value={username} type="username" onChange={this.handleChange}/>
+                  </div>
+                  <div class="form-group has-error">
+                    <input class="form-control input-lg" placeholder="E-mail Address" value={email} name="email" type="text" onChange={this.handleChange}/>
+                  </div>
+                  <div class="form-group has-success">
+                    <input class="form-control input-lg" placeholder="Password" name="password" value={password} type="password" onChange={this.handleChange}/>
+                  </div>
+                  <div class="form-group has-success">
+                    <input class="form-control input-lg" placeholder="Confirm Password" name="password" value={password} type="password" onChange={this.handleChange}/>
+                  </div>
+                  <div class="checkbox">
+                    <label class="small">
+                      <input name="terms" type="checkbox"/>I have read and agree to the <a href="#">terms of service</a>
+                    </label>
+                  </div>
+                    <input class="btn btn-lg btn-primary btn-block" value="Sign Me Up" type="submit"/>
+                </fieldset>
+            </div>
+          </div>
+        </div>
       </div>
+
     )
   }
 }
