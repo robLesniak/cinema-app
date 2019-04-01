179170693ce668c6906b11d66ff01e599d334829
Michał Nowakowski <michal951@poczta.onet.pl>
2019-04-01 21:26:32 +0200

    create movie dashboard item

---
+++ client/src/components/MovieDashobard/Dashboard.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <div>elo</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => console.log(state)({});
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "films" }])
+)(Dashboard);
