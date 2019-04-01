1bc8071a048bde1593070889a67dde085b12e80a
Michał Nowakowski <michal951@poczta.onet.pl>
2019-04-02 00:01:28 +0200

    add firestoreReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import movieReducer from "./movieReducer";
+import { firestoreReducer } from "redux-firestore";
 
 export default combineReducers({
-  movie: movieReducer
+  movie: movieReducer,
+  firestore: firestoreReducer
 });
