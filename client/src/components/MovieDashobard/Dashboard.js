3f9ff5766c491270a149b2d431f6212a89c199c5
Michał Nowakowski <michal951@poczta.onet.pl>
2019-04-01 23:58:54 +0200

    component now display all movies in cinema

--- client/src/components/MovieDashobard/Dashboard.js
+++ client/src/components/MovieDashobard/Dashboard.js
@@ -2,18 +2,34 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import MovieList from "./Movie/MovieList";
 
 class Dashboard extends Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div>
-        <div>elo</div>
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col col-lg-2" />
+          <div
+            class="col-md-auto"
+            style={{ marginTop: "5px", marginBottom: "5px" }}
+          >
+            <MovieList movies={movies} />
+          </div>
+          <div class="col col-lg-2" />
+        </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    movies: state.firestore.ordered.films
+  };
+};
 
 export default compose(
   connect(mapStateToProps),
