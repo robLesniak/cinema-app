556de4404ee6591dbab95353813334da56f0fa85
Michał Nowakowski <michal951@poczta.onet.pl>
2019-04-01 23:59:32 +0200

    functional component to display proper movie properties

--- client/src/components/MovieDashobard/Movie/MovieItem.js
+++ client/src/components/MovieDashobard/Movie/MovieItem.js
@@ -1,7 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default class MovieItem extends Component {
-  render() {
-    return <div />;
-  }
-}
+const MovieItem = ({ movie }) => {
+  return (
+    <div className="card" style={{ marginBottom: "5px" }}>
+      <div
+        className="card-header"
+        style={{
+          backgroundColor: "white",
+          fontSize: "25px",
+          fontFamily: "Comic Sans MS"
+        }}
+      >
+        {movie.title} ({movie.Year})
+      </div>
+      <div className="row ">
+        <div className="col-md-4">
+          <img className="" src={movie.image} alt="" />
+        </div>
+        <div className="col-md-8 px-3">
+          <div className="card-block px-6">
+            <h4 className="card-title">{movie.type}</h4>
+            <p className="card-tex">{movie.plot}</p>
+            <p className="card-text">Director: {movie.director}</p>
+            <p className="card-text">Duration: {movie.duration} mins </p>
+            <Link
+              to=""
+              className="btn btn-lg"
+              style={{ backgroundColor: "#7070EF" }}
+            >
+              Movie details
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieItem;
