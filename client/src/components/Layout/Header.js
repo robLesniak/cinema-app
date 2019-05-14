04ba30b3cf5cb4c381914a08f1f659f22a98aae5
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-14 12:12:52 +0200

    update header

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -12,20 +12,38 @@ class Header extends Component {
               className="nav-link "
               style={{ fontSize: "20px" }}
             >
-              <i className="fa fa-film" />
-              &nbsp;Repertoire
+              <button
+                type="button"
+                className="btn btn-lg btn-outline-light "
+                style={{ border: "none" }}
+              >
+                <i className="fa fa-film" />
+                &nbsp;Repertoire
+              </button>
             </Link>
           </li>
           <li className="nav-item nav-divider">
             <Link to="" className="nav-link" style={{ fontSize: "20px" }}>
-              <i className="fa fa-money" />
-              &nbsp;Price list
+              <button
+                type="button"
+                className="btn btn-lg btn-outline-light "
+                style={{ border: "none" }}
+              >
+                <i className="fa fa-money" />
+                &nbsp;Price list
+              </button>
             </Link>
           </li>
           <li className="nav-item">
             <Link to="" className="nav-link" style={{ fontSize: "20px" }}>
-              About us&nbsp;
-              <i className="fa fa-question-circle-o" />
+              <button
+                type="button"
+                className="btn btn-lg btn-outline-light "
+                style={{ border: "none" }}
+              >
+                About us&nbsp;
+                <i className="fa fa-question-circle-o" />
+              </button>
             </Link>
           </li>
         </ul>
@@ -36,14 +54,24 @@ class Header extends Component {
               className="nav-link "
               style={{ fontSize: "20px" }}
             >
-              <button type="button" className="btn btn-lg btn-outline-light ">
+              <button
+                type="button"
+                className="btn btn-lg btn-outline-light "
+                style={{ border: "none" }}
+              >
                 Sing up
               </button>
             </Link>
           </li>
           <li className="nav-item">
             <Link to="/login" className="nav-link" style={{ fontSize: "20px" }}>
-              <button type="button" className="btn btn-lg btn-outline-light ">
+              <button
+                type="button"
+                className="btn btn-lg btn-outline-light "
+                style={{
+                  border: "none"
+                }}
+              >
                 Login
               </button>
             </Link>
@@ -55,7 +83,7 @@ class Header extends Component {
     return (
       <nav
         className="navbar navbar-expand-sm navbar-light "
-        style={{ backgroundColor: "#7070EF" }}
+        style={{ backgroundColor: "#4b4bf4" }}
       >
         <div className="container">
           <Link to="" className="navbar-brand">
