f403868d89bd1a774b35172d2e37d34e0dd718ba
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-14 12:11:00 +0200

    add two new routes for details and avaiability

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Register from "./components/UserManagement/Register";
 import AddMovie from "./components/MovieDashobard/Movie/AddMovie";
 import Dashboard from "./components/MovieDashobard/Dashboard";
 import Login from "./components/UserManagement/Login";
+import MovieAvaiability from "./components/MovieDashobard/Movie/MovieAvaiability";
+import MovieDetails from "./components/MovieDashobard/Movie/MovieDetails";
 
 class App extends Component {
   render() {
@@ -22,7 +24,16 @@ class App extends Component {
             <Route exact path="/register" component={Register} />
             <Route exact path="/repertoire" component={Dashboard} />
             <Route exact path="/Login" component={Login} />
-            <Route exact path={"/repertoire/:movieId"} />
+            <Route
+              exact
+              path={"/repertoire/:movieId"}
+              component={MovieAvaiability}
+            />
+            <Route
+              exact
+              path={"/repertoire/:movieId/details"}
+              component={MovieDetails}
+            />
           </div>
         </Router>
       </Provider>
