b7e7a7f54c09be142f3d64aadad009c56b66a173
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-18 21:37:55 +0200

    add case for sign in and for singin errors

--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,37 +1,48 @@
-import {  SIGNUP_SUCCES, 
-          SIGNUP_ERROR, 
-          SIGNIN_SUCCES,
-          SIGNIN_ERROR  } from "../actions/types";
+import {
+  SIGNUP_SUCCES,
+  SIGNUP_ERROR,
+  SIGNIN_SUCCES,
+  SIGNIN_ERROR,
+  SIGNOUT_SUCCESS
+} from "../actions/types";
 
 const initialState = {
-  Users: [],
+  authError: null,
   User: {}
 };
 
-export default function (state = initialState, action) {
+const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case SIGNUP_SUCCES:
-      console.log('signup succ');
+      console.log("signup succ");
       return {
         ...state,
         User: action.payload
       };
     case SIGNUP_ERROR:
       return {
-        ...state,
-      }
-    case SIGNIN_SUCCES:
-    console.log('signin succ');
-      return {
-        ...state,
-        User: action.payload
-      }
-    case SIGNIN_ERROR:
-    console.log('signin error');
-      return {
         ...state
-      }
+      };
+    case SIGNIN_SUCCES:
+      console.log("signin succ");
+      return {
+        ...state,
+        User: action.payload,
+        authError: null
+      };
+
+    case SIGNIN_ERROR:
+      console.log("signin error");
+      return {
+        ...state,
+        authError: "Login failed"
+      };
+    case SIGNOUT_SUCCESS:
+      console.log("log out");
+      return { state };
     default:
       return state;
   }
-}
+};
+
+export default authReducer;
