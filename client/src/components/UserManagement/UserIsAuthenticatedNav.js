d6df28f482559cb28b3e9d319953c23c0a431320
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-18 21:32:37 +0200

    create two functional components for non authicanted and authenicated users

---
+++ client/src/components/UserManagement/UserIsAuthenticatedNav.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
+
+import { Link } from "react-router-dom";
+
+const UserIsAuthenticatedNav = props => {
+  const { auth } = props;
+  return (
+    <div>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link " style={{ fontSize: "20px" }}>
+              <button
+                type="button"
+                className="btn btn-lg btn-outline-light "
+                style={{ border: "none", fontWeight: "bold" }}
+              >
+                {auth.email.substring(0, auth.email.indexOf("@"))}
+                &nbsp;
+                <i class="fa fa-user-circle" />
+              </button>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link" style={{ fontSize: "20px" }}>
+              <button
+                type="button"
+                className="btn btn-lg btn-outline-light "
+                style={{
+                  border: "none",
+                  fontWeight: "bold"
+                }}
+                onClick={props.signOut}
+              >
+                Logout
+              </button>
+            </Link>
+          </li>
+          <li className="nav-item" />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserIsAuthenticatedNav);
