86001b52003f99e1dc5d26a7e44e3c7677020cf8
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-18 17:05:14 +0200

    minor change

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import Header from "./components/Layout/Header";
-import Footer from "./components/Layout/Footer";
 import Register from "./components/UserManagement/Register";
 import AddMovie from "./components/MovieDashobard/Movie/AddMovie";
 import Dashboard from "./components/MovieDashobard/Dashboard";
