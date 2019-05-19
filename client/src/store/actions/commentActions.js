f995ee39f98f62040e01d44e2f7eda20bee8e996
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-19 23:11:36 +0200

    add to collections 2 new fields authorUsername and authorId

--- client/src/store/actions/commentActions.js
+++ client/src/store/actions/commentActions.js
@@ -1,8 +1,12 @@
 export const createComment = comment => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const uid = getState().firebase.auth.uid;
     firestore.collection("comments").add({
       ...comment,
+      authorUsername: profile.username,
+      authorId: uid,
       createdAt: new Date()
     });
     dispatch({
