8387967aad1e3637a40b3766b3a7b8af84fcaa23
Mario <dddd@dasdasdwd.sdas>
2019-05-21 11:07:18 +0200

    add API files

---
+++ BaseAPI/BaseAPI/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using BaseAPI.Models;
+
+
+namespace BaseAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddCors();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<ClientsContext>(options =>
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else      
+                app.UseHsts();
+
+            app.UseMvc();
+        }
+    }
+}
