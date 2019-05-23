225e0898e2c9dbeb9fbbe7d8aa198bd5d7a50491
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-24 00:18:27 +0200

    complete refactor add movie to use external api

--- client/src/components/MovieDashobard/Movie/AddMovie.js
+++ client/src/components/MovieDashobard/Movie/AddMovie.js
@@ -15,9 +15,13 @@ class AddMovie extends Component {
       plot: "",
       title: "",
       type: "",
+      trailerURL: "",
       movies: [],
       genres: [],
-      selectedMovie: ""
+      selectedMovie: "",
+      writer: "",
+      avaiability: "",
+      movie: null
     };
   }
 
@@ -45,8 +49,81 @@ class AddMovie extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  generateSelectedMovie = () => {
-    return <div />;
+  generateSelectedMovie = id => {
+    axios.get(`http://51.15.102.229:5000/api/movies/${id}`).then(res => {
+      const movie = res.data;
+      this.setState({ movie: movie });
+      this.setState({ plot: movie[0].description });
+      this.setState({ trailerURL: movie[0].trailers[0].trailerURL });
+      this.setState({ Year: movie[0].releaseDate.substring(0, 4) });
+      const type =
+        "" +
+        movie[0].genres.map(genre => {
+          return genre.genreType + " ";
+        });
+
+      this.setState({
+        type: type.substring(0, type.lastIndexOf(","))
+      });
+      this.setState({ title: movie[0].title });
+      this.setState({ image: movie[0].posters[0].posterLink });
+      this.setState({ duration: movie[0].duration });
+      const fullName = movie[0].role.map(rol => {
+        return rol.roleDesc === "director"
+          ? rol.personInformation.personFirstName +
+              " " +
+              rol.personInformation.personLastName +
+              " "
+          : "";
+      });
+      const director = fullName.filter(function(el) {
+        return el !== "";
+      });
+
+      this.setState({
+        director: director
+      });
+
+      const acfullName = movie[0].role.map(rol => {
+        return rol.roleDesc === "lead actor" || rol.roleDesc === "co-actor"
+          ? rol.personInformation.personFirstName +
+              " " +
+              rol.personInformation.personLastName +
+              " "
+          : "";
+      });
+
+      const actors = acfullName.filter(function(el) {
+        return el !== "";
+      });
+      this.setState({
+        actors: actors
+      });
+
+      const wrfullName = movie[0].role.map(rol => {
+        return rol.roleDesc === "writer"
+          ? rol.personInformation.personFirstName +
+              " " +
+              rol.personInformation.personLastName +
+              " "
+          : "";
+      });
+
+      const writers = wrfullName.filter(function(el) {
+        return el !== "";
+      });
+
+      this.setState({ writer: writers });
+    });
+    axios.get(`http://51.15.102.229:5000/api/seans/${id}`).then(res => {
+      const data = res.data;
+      this.setState({ avaiability: data.seanceDate });
+    });
+  };
+
+  onSelectChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    this.generateSelectedMovie(e.target.value);
   };
 
   onSubmit = e => {
@@ -59,7 +136,10 @@ class AddMovie extends Component {
       image: this.state.image,
       plot: this.state.plot,
       title: this.state.title,
-      type: this.state.type
+      type: this.state.type,
+      writer: this.state.writer,
+      trailerURL: this.state.trailerURL,
+      avaiability: this.state.avaiability
     };
 
     this.props.createMovie(newMovie, this.props.history);
@@ -68,17 +148,18 @@ class AddMovie extends Component {
   render() {
     return (
       <div className="movie">
-        {console.log(this.state.movies)}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
+              <h1 className="display-5 text-center">Add Movie</h1>
               <select
                 className="custom-select custom-select-lg mb-3"
                 placeholder="select movie to add"
                 name="selectedMovie"
-                onChange={this.onChange}
+                onChange={this.onSelectChange}
+                style={{ marginTop: "5px" }}
               >
-                <option value="" disabled selected hidden>
+                <option value="" disabled defaultValue hidden>
                   {" "}
                   Choose movie to add
                 </option>
@@ -100,25 +181,34 @@ class AddMovie extends Component {
                 }
               </select>
               {this.state.selectedMovie !== "" ? <div /> : null}
-              <h1 className="display-5 text-center">Add Movie</h1>
+
               <hr />
+              {this.state.selectedMovie !== "" ? null : null}
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg "
-                    placeholder="Movie title"
+                    placeholder={
+                      this.state.title ? this.state.title : "Movie title"
+                    }
                     name="title"
-                    onChange={this.onChange}
+                    disabled
+                    style={{ textAlign: "center" }}
                   />
                 </div>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg "
-                    placeholder="Director"
+                    placeholder={
+                      this.state.director
+                        ? this.state.director
+                        : "Movie director"
+                    }
                     name="director"
-                    onChange={this.onChange}
+                    style={{ textAlign: "center" }}
+                    disabled
                   />
                 </div>
 
@@ -126,61 +216,96 @@ class AddMovie extends Component {
                   <input
                     type="text"
                     className="form-control form-control-lg "
-                    placeholder="Main actors"
+                    placeholder={
+                      this.state.actors ? this.state.actors : "Main actors"
+                    }
                     name="actors"
-                    onChange={this.onChange}
+                    style={{ textAlign: "center" }}
+                    disabled
                   />
                 </div>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg "
-                    placeholder="Type"
+                    placeholder={
+                      this.state.type ? this.state.type : "Movie genre"
+                    }
                     name="type"
-                    onChange={this.onChange}
+                    style={{ textAlign: "center" }}
+                    disabled
                   />
                 </div>
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg"
-                    placeholder="Year of publishment"
+                    placeholder={
+                      this.state.Year ? this.state.Year : "year of release"
+                    }
                     name="Year"
-                    onChange={this.onChange}
+                    style={{ textAlign: "center" }}
+                    disabled
                   />
                 </div>
                 <div className="form-group">
                   <input
                     type="number"
                     className="form-control form-control-lg "
-                    placeholder="Duration in mins"
+                    placeholder={
+                      this.state.duration ? this.state.duration : "duration"
+                    }
                     name="duration"
-                    onChange={this.onChange}
+                    style={{ textAlign: "center" }}
+                    disabled
                   />
                 </div>
                 <div className="form-group">
                   <textarea
                     className="form-control form-control-lg"
-                    placeholder="Movie plot"
+                    rows="2"
+                    placeholder={
+                      this.state.plot ? this.state.plot : "Movie plot"
+                    }
                     name="plot"
-                    onChange={this.onChange}
+                    style={{ textAlign: "center" }}
+                    disabled
                   />
                 </div>
-
                 <div className="form-group">
                   <input
                     type="text"
                     className="form-control form-control-lg"
-                    placeholder="Poster image"
-                    name="image"
-                    onChange={this.onChange}
+                    placeholder={
+                      this.state.trailerURL
+                        ? this.state.trailerURL
+                        : "Link to movie trailer"
+                    }
+                    name="trailerURL"
+                    style={{ textAlign: "center" }}
+                    disabled
                   />
+                </div>
+                <div className="form-group">
+                  {this.state.image ? (
+                    <img src={this.state.image} alt="" />
+                  ) : (
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Movie poster"
+                      name="image"
+                      style={{ textAlign: "center" }}
+                      disabled
+                    />
+                  )}
                 </div>
 
                 <input
                   type="submit"
                   className="btn btn-block mt-4"
-                  style={{ backgroundColor: "#7070EF" }}
+                  placeholder="Add movie"
+                  style={{ backgroundColor: "#7070EF", marginBottom: "5px" }}
                 />
               </form>
             </div>
