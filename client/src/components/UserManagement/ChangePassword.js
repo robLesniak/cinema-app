782e3cdc69a33b3a137caf7eaf20967ab67fade8
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-24 22:48:40 +0200

    add redirect after succesfull change of password

--- client/src/components/UserManagement/ChangePassword.js
+++ client/src/components/UserManagement/ChangePassword.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import firebase from '../../config/firebaseConfig';
-
+import React, { Component } from "react";
+import firebase from "../../config/firebaseConfig";
+import { Redirect } from "react-router-dom";
 class ChangePassword extends Component {
-  state = {
-    currentPassword: '',
-    newPassword: '',
-    passwordColor: '',
-    errorPassword: null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPassword: "",
+      newPassword: "",
+      passwordColor: "",
+      errorPassword: null
+    };
   }
-
 
   handleChange = event => {
     this.setState({
@@ -20,29 +22,38 @@ class ChangePassword extends Component {
     this.setState({ passwordColor: "#d1d1fa" });
   };
 
-
-  reauthenticate = (currentPassword) => {
+  reauthenticate = currentPassword => {
     let user = firebase.auth().currentUser;
-    let cred = firebase.auth.EmailAuthProvider.credential(user.email, currentPassword);
+    let cred = firebase.auth.EmailAuthProvider.credential(
+      user.email,
+      currentPassword
+    );
     return user.reauthenticateAndRetrieveDataWithCredential(cred);
-  }
+  };
 
   changePassword = () => {
-    this.reauthenticate(this.state.currentPassword).then(() => {
-      let user = firebase.auth().currentUser;
-      user.updatePassword(this.state.newPassword).then((error) => {
-        this.setState({ errorPassword: `Password has been changed`})
-      }).catch((error) => { this.setState({ errorPassword: error.message}) });
-    }).catch((error) => { this.setState({ errorPassword: error.message})});
-  }
-
+    this.reauthenticate(this.state.currentPassword)
+      .then(() => {
+        let user = firebase.auth().currentUser;
+        user
+          .updatePassword(this.state.newPassword)
+          .then(error => {
+            this.setState({ errorPassword: `Password has been changed` });
+          })
+          .catch(error => {
+            this.setState({ errorPassword: error.message });
+          });
+      })
+      .catch(error => {
+        this.setState({ errorPassword: error.message });
+      });
+  };
 
   render() {
-    const { 
-      currentPassword,
-      newPassword, 
-      errorPassword, 
-    } = this.state
+    console.log(firebase.auth().currentUser);
+    if (this.state.errorPassword === "Password has been changed") {
+      return <Redirect to="/repertoire" />;
+    }
     return (
       <div className="container-fluid bg-light py-3">
         <div className="row">
@@ -50,55 +61,63 @@ class ChangePassword extends Component {
             <div className="card card-body">
               <div
                 className="text-center "
-                style={{ marginTop: "5px", marginBottom: "13px", fontSize: "2rem"}}
-              >Change your password</div>
-                <div className="form-group has-success">
-                  <input 
-                    className="form-control input-lg"
-                    type="password" 
-                    placeholder="Current Password" 
-                    name="currentPassword"
-                    value={currentPassword}
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <div className="form-group has-success">
-                  <input 
-                    className="form-control input-lg"
-                    type="password" 
-                    placeholder="New Password"
-                    name="newPassword"
-                    value={newPassword}
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <button 
-                  className="btn btn-lg btn-outline-light"
-                  onClick={this.changePassword}
-                    style={{
-                      backgroundColor: "#7070EF",
-                      marginLeft: "5px",
-                      marginBottom: "5px",
-                      border: "none",
-                      marginTop: "5px",
-                      fontWeight: "bold"
-                    }}
-                >Submit</button>
-                <h5 
-                  style={{ 
-                    color: "blue", 
-                    fontWeight: "bold",
-                    marginTop: "10px"
-                  }}
-                >
-                  {errorPassword}
-                </h5>
+                style={{
+                  marginTop: "5px",
+                  marginBottom: "13px",
+                  fontSize: "2rem"
+                }}
+              >
+                Change your password
               </div>
+              <div className="form-group has-success">
+                <input
+                  className="form-control input-lg"
+                  type="password"
+                  placeholder="Current Password"
+                  name="currentPassword"
+                  value={this.state.currentPassword}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group has-success">
+                <input
+                  className="form-control input-lg"
+                  type="password"
+                  placeholder="New Password"
+                  name="newPassword"
+                  value={this.state.newPassword}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button
+                className="btn btn-lg btn-outline-light"
+                onClick={this.changePassword}
+                style={{
+                  backgroundColor: "#7070EF",
+                  marginLeft: "5px",
+                  marginBottom: "5px",
+                  border: "none",
+                  marginTop: "5px",
+                  fontWeight: "bold"
+                }}
+              >
+                Submit
+              </button>
+              <h5
+                style={{
+                  color: "blue",
+                  fontWeight: "bold",
+                  marginTop: "10px"
+                }}
+              >
+                {this.state.errorPassword}
+              </h5>
             </div>
           </div>
         </div>
-    )
+      </div>
+    );
   }
 }
 
-export default  ChangePassword;
+export default ChangePassword;
