9493901e35b348a7ebc6c564332ec506c1d60b86
Michał Nowakowski <michal951@poczta.onet.pl>
2019-05-24 22:47:39 +0200

    change link to change password and if user is logged by FB the dropdown item for chaning password is null

--- client/src/components/UserManagement/UserIsAuthenticatedNav.js
+++ client/src/components/UserManagement/UserIsAuthenticatedNav.js
@@ -9,51 +9,63 @@ import Avatar from "react-avatar";
 const UserIsAuthenticatedNav = props => {
   // const icon = <i class="fa fa-user-circle" />;
   const { auth, profile } = props;
+  console.log(profile);
   return (
-        <ul className="navbar-nav ml-auto" style={{ alignItems: "center" }}>
-          <li className="nav-item">
-          <div className="dropdown">
-            {/* <Link to="/" className="nav-link " style={{ fontSize: "20px" }}> */}
-              <button
-                type="button"
-                className="btn btn-lg btn-outline dropdown-toggle"
-                id="dropdownMenuButton" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false"
-                style={{ border: "none", fontWeight: "bold" }}
-              >
-                {profile.username ? (
-                  profile.username
-                ) : (
-                  <Avatar
-                    facebookId={auth.providerData[0].uid}
-                    size="50"
-                    round={true}
-                  />
-                )}
-              </button>
-            {/* </Link> */}
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <Link className="dropdown-item" to="/user/:id/reservation">Reservation</Link>
-              <Link className="dropdown-item" to="/user/changePassword">Change password</Link>
-            </div>
-            </div>
-          </li>
-          <li className="nav-item">
-            <Link to="/login" className="nav-link" style={{ fontSize: "20px" }}>
-              <button
-                type="button"
-                className="buttonHover btn btn-lg btn-outline"
-                style={{
-                  border: "none",
-                  fontWeight: "bold"
-                }}
-                onClick={props.signOut}
-              >
-                Logout
-              </button>
+    <ul className="navbar-nav ml-auto" style={{ alignItems: "center" }}>
+      <li className="nav-item">
+        <div className="dropdown">
+          {/* <Link to="/" className="nav-link " style={{ fontSize: "20px" }}> */}
+          <button
+            type="button"
+            className="btn btn-lg btn-outline dropdown-toggle"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            style={{ border: "none", fontWeight: "bold" }}
+          >
+            {profile.username ? (
+              profile.username
+            ) : (
+              <Avatar
+                facebookId={auth.providerData[0].uid}
+                size="50"
+                round={true}
+              />
+            )}
+          </button>
+          {/* </Link> */}
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <Link className="dropdown-item" to="/user/:id/reservation">
+              Reservation
             </Link>
-          </li>
-        </ul>
+            {auth.providerData[0].providerId === "facebook.com" ? null : (
+              <Link
+                className="dropdown-item"
+                to={`/user/${auth.uid}/changePassword`}
+              >
+                Change password
+              </Link>
+            )}
+          </div>
+        </div>
+      </li>
+      <li className="nav-item">
+        <Link to="/login" className="nav-link" style={{ fontSize: "20px" }}>
+          <button
+            type="button"
+            className="buttonHover btn btn-lg btn-outline"
+            style={{
+              border: "none",
+              fontWeight: "bold"
+            }}
+            onClick={props.signOut}
+          >
+            Logout
+          </button>
+        </Link>
+      </li>
+    </ul>
   );
 };
 
