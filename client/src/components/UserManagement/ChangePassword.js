1081998a409861e67468c56a54ca71a03212d449
Robert Lesniak <robert.lesniak@thecogworks.com>
2019-05-24 18:13:16 +0200

    changed password feature finished

--- client/src/components/UserManagement/ChangePassword.js
+++ client/src/components/UserManagement/ChangePassword.js
@@ -6,6 +6,7 @@ class ChangePassword extends Component {
     currentPassword: '',
     newPassword: '',
     passwordColor: '',
+    errorPassword: null,
   }
 
 
@@ -23,34 +24,77 @@ class ChangePassword extends Component {
   reauthenticate = (currentPassword) => {
     let user = firebase.auth().currentUser;
     let cred = firebase.auth.EmailAuthProvider.credential(user.email, currentPassword);
-    console.log(user.email);
     return user.reauthenticateAndRetrieveDataWithCredential(cred);
   }
 
   changePassword = () => {
     this.reauthenticate(this.state.currentPassword).then(() => {
       let user = firebase.auth().currentUser;
-      user.updatePassword(this.state.newPassword).then(() => {
-        console.log('password has been changed')
-      }).catch((error) => { console.log(error.message); });
-    }).catch((error) => { let invalid =  error.message });
+      user.updatePassword(this.state.newPassword).then((error) => {
+        this.setState({ errorPassword: `Password has been changed`})
+      }).catch((error) => { this.setState({ errorPassword: error.message}) });
+    }).catch((error) => { this.setState({ errorPassword: error.message})});
   }
 
 
   render() {
-    const { currentPassword, newPassword } = this.state
+    const { 
+      currentPassword,
+      newPassword, 
+      errorPassword, 
+    } = this.state
     return (
-      <div>
-        <input type="password" placeholder="Current Password" name="currentPassword"
-          value={currentPassword}
-          onChange={this.handleChange}
-        />
-        <input type="password" placeholder="New Password" name="newPassword"
-          value={newPassword}
-          onChange={this.handleChange}
-        />
-        <button onClick={this.changePassword}>button</button>
-      </div>
+      <div className="container-fluid bg-light py-3">
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            <div className="card card-body">
+              <div
+                className="text-center "
+                style={{ marginTop: "5px", marginBottom: "13px", fontSize: "2rem"}}
+              >Change your password</div>
+                <div className="form-group has-success">
+                  <input 
+                    className="form-control input-lg"
+                    type="password" 
+                    placeholder="Current Password" 
+                    name="currentPassword"
+                    value={currentPassword}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group has-success">
+                  <input 
+                    className="form-control input-lg"
+                    type="password" 
+                    placeholder="New Password"
+                    name="newPassword"
+                    value={newPassword}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <button 
+                  className="btn btn-lg btn-outline-light"
+                  onClick={this.changePassword}
+                    style={{
+                      backgroundColor: "#7070EF",
+                      marginLeft: "5px",
+                      marginBottom: "5px",
+                      border: "none",
+                      marginTop: "5px",
+                      fontWeight: "bold"
+                    }}
+                >Submit</button>
+                <h5 
+                  style={{ 
+                    color: "blue", 
+                    fontWeight: "bold" }}
+                >
+                  {errorPassword}
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
     )
   }
 }
